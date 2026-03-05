ec98186322d61424ede790d41ae87d832433f354
An <nguyenquangan318@gmail.com>
2026-03-05 07:14:56 +0700

    JS resource

---
+++ Session 01/Demo/script.js
@@ -0,0 +1,29 @@
+//Biến
+var number = 1;
+let string = "Hello";
+const bool = false;
+
+//Kiểu dữ liệu
+let myString = `Hello`;
+
+
+//Input output
+// let inputName = prompt("Nhập tên của bạn");
+// alert(inputName);
+// let inputNum = +prompt("Nhập số nguyên bất kỳ");
+// console.log(`Tổng của ${inputNum} và 3 là: ${inputNum + 3}`);
+
+
+//Math object
+// console.log(Math);
+console.log((Math.random() * 1000) + 1)
+
+//Date objects
+// let currentDate = new Date();
+// console.log(currentDate.getFullYear());
+
+
+
+
+
+
---
+++ Session 14/BTTH-IT103B-Session-14/script.js
@@ -0,0 +1,49 @@
+const products = [
+    { id: 1, name: "Bánh Chưng", price: 150000, img: "./img/banhchung.webp" },
+    { id: 2, name: "Giò Lụa", price: 180000, img: "./img/giolua.jpg" },
+    { id: 3, name: "Cành Đào", price: 500000, img: "./img/canhdao.webp" },
+    { id: 4, name: "Mứt Tết", price: 120000, img: "./img/muttet.webp" },
+    { id: 5, name: "Lì Xì (Tệp)", price: 20000, img: "./img/lixi.webp" },
+    { id: 6, name: "Dưa Hấu", price: 60000, img: "./img/duahau.jpg" }
+];
+
+const cart = [
+    { id: 1, name: "Bánh Chưng", price: 150000 },
+    { id: 2, name: "Giò Lụa", price: 180000 }
+]
+
+function renderProducts() {
+    const productsList = document.getElementById("product-list");
+    products.forEach((product) => {
+        productsList.innerHTML += `<div class="product-card">
+                    <img src="${product.img}" alt="">
+                    <h3>${product.name}</h3>
+                    <p class="price">${product.price.toLocaleString("vi-vn")}đ</p>
+                    <button class="btn-add" id="">Thêm vào giỏ</button>
+                </div>`
+    })
+}
+
+function renderCart() {
+    const cartList = document.getElementsByClassName("cart-list")[0];
+    cart.forEach((item) => {
+        cartList.innerHTML += `<li>
+                        <span class="cart-item-name">${item.name}</span>
+                        <div>
+                            <span class="cart-item-price">${item.price.toLocaleString("vi-vn")}đ</span>
+                            <button class="btn-remove">X</button>
+                        </div>
+                    </li>`
+    })
+
+    const emptyStr = document.getElementsByClassName("empty-msg")[0];
+    if (cart.length !== 0) {
+        emptyStr.style.display = "none";
+    }
+
+    const totalElement = document.getElementById("total-price");
+    totalElement.innerText = cart.reduce((total, item) => total + item.price, 0).toLocaleString("vi-vn")+ "đ";
+}
+
+renderProducts();
+renderCart();
